fca23a8df06c137f4b6feed10a29758236e0dec5
ioleksiu <ilia041@gmail.com>
2017-05-08 17:47:29 +0300

    filler

--- send_cord.c
+++ send_cord.c
@@ -6,7 +6,7 @@
 /*   By: ioleksiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 13:21:55 by ioleksiu          #+#    #+#             */
-/*   Updated: 2017/05/07 19:01:21 by ioleksiu         ###   ########.fr       */
+/*   Updated: 2017/05/08 17:46:28 by ioleksiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	send_cord_xy(t_filler *f)
 {
+	f->can_place_figure = (f->xy_cord_send[0] == -42) ? 0 : 1;
 	f->xy_cord_send[0] = (f->xy_cord_send[0] == -42) ? 0 : (f->xy_cord_send[0]);
 	f->xy_cord_send[1] = (f->xy_cord_send[1] == -42) ? 0 : (f->xy_cord_send[1]);
-	f->can_place_figure = (f->xy_cord_send[0] == -42) ? 0 : 1;
 	ft_putnbr_fd(f->xy_cord_send[0], 1);
 	write(1, " ", 1);
 	ft_putnbr_fd(f->xy_cord_send[1], 1);
